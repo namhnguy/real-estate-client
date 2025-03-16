e34dbd21bab4762c8c0f1b95b882a937a5f25bf3
namhnguy-home <namhnguy@gmail.com>
2025-03-15 19:59:10 -0500

    initial + finish landing

---
+++ src/app/(nondashboard)/landing/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import HeroSection from "./HeroSection";
+import FeatureSection from "./FeatureSection";
+import DiscoverSection from "./DiscoverSection";
+import CallToActionSection from "./CallToActionSection";
+import FooterSection from "./FooterSection";
+
+const Landing = () => {
+  return (
+    <div>
+      <HeroSection />
+      <FeatureSection />
+      <DiscoverSection />
+      <CallToActionSection />
+      <FooterSection />
+    </div>
+  );
+};
+
+export default Landing;
